80e9c8938fe6411462d910179f802ca50d61cfd1
Samuel <samuel@mknd-records.com>
2023-02-07 16:04:23 +0100

    Ajout bonus Changer l’ordre des profils

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@
 
 import * as User from "./modules/User.js";
 
+let dataUsers;
+
 const getRandomUsers = async () => {
     const response = await(fetch("https://randomuser.me/api/?results=20"));
     const data = await response.json();
@@ -38,7 +40,7 @@ const getRandomUsers = async () => {
  */
 const createUsers = async () => {
     const usersPromise = await getRandomUsers();
-    console.log(usersPromise);
+    dataUsers = usersPromise;
     usersPromise.forEach((user) => {
         const userObject = new User.User(user.first,user.last, user.age, user.email, user.picture, user.city, user.country, user.title);
         userObject.render();
@@ -46,4 +48,69 @@ const createUsers = async () => {
     );
 }
 
-createUsers();
+/*
+ * Order users by name
+ */
+const orderUsersByName = (map) => {
+    map.sort((a, b) => {
+        if (a.last < b.last) {
+            return -1;
+        }
+        if (a.last > b.last) {
+            return 1;
+        }
+        return 0;
+    });
+    return map
+}
+
+
+/*
+ * Order users by age
+ */
+const orderUsersByAge = (map) => {
+    map.sort((a, b) => {
+        if (a.age < b.age) {
+            return -1;
+        }
+        if (a.age > b.age) {
+            return 1;
+        }
+        return 0;
+    });
+    return map
+}
+
+/*
+ * update render
+ */
+const updateRender = (map) => {
+    document.querySelector("main").innerHTML = "";
+    map.forEach((user) => {
+        const userObject = new User.User(user.first,user.last, user.age, user.email, user.picture, user.city, user.country, user.title);
+        userObject.render();
+    }
+    );
+}
+
+
+createUsers().catch((error) => {
+    console.error(error);
+});
+
+
+/*
+ * Events listener for button with id id="sort--name"
+ */
+document.querySelector("#sort--name").addEventListener("click", () => {
+    //change class="selected" to the button clicked
+    document.querySelector(".selected").classList.remove("selected");
+    document.querySelector("#sort--name").classList.add("selected");
+    updateRender(orderUsersByName(dataUsers));
+});
+document.querySelector("#sort--age").addEventListener("click", () => {
+    document.querySelector(".selected").classList.remove("selected");
+    document.querySelector("#sort--age").classList.add("selected");
+    updateRender(orderUsersByAge(dataUsers));
+});
+
